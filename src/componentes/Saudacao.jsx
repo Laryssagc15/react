4350e2b80e2baa620001e94088b5620ae2e89eb6
Laryssagc15 <laryssagomesc1313@gmail.com>
2019-08-06 14:13:45 -0300

    react

--- src/componentes/Saudacao.jsx
+++ src/componentes/Saudacao.jsx
@@ -1,16 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 //Component de classe que é usado para ser endado
-//A classe se torna um componente
-export default class saudacao extends Component{
-  render(){
-    const { tipo, nome } = this.props
+//A classe se torna um component
+export default class saudacao extends Component {
+  //state é para mostrar o que está acontecendo em cima.
+  state = {
+    tipo: this.props.tipo,
+    nome: this.props.nome
+  };
+  //Para receber os valores digitados
+  setTipo(e) {
+    this.setState({ tipo: e.target.value });
+  }
+
+  setNome(e) {
+    this.setState({ nome: e.target.value });
+  }
+
+  render() {
+    const { tipo, nome } = this.state;
     return (
       <div>
-        <h1>{ tipo }{ nome }</h1>
-        <hr  />
-        <input type = "text" placeholder = "Tipo..." value = {tipo} />
-        <input type = "text" placrholder = "Nome..." value = {nome} />
+        <h1>
+          {tipo}
+          {nome}
+        </h1>
+        <hr />
+        <input type="text" placeholder="Tipo..." value={tipo} onChange={e => this.setTipo(e)} />
+        <input type="text" placeholder="Nome..." value={nome} onChange={e => this.setNome(e)} />
       </div>
-     )
-   }
+    );
+  }
 }
